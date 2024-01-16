d1c4550206779a01f6796c234fd51785c7858256
BenjaminDiazP <20223tn048@utez.edu.mx>
2024-01-16 13:27:56 -0600

    Subi la base para un java fx

---
+++ base-proyecto5b/src/main/java/mx/edu/utez/baseproyecto5b/controller/HelloView.java
@@ -0,0 +1,19 @@
+package mx.edu.utez.baseproyecto5b.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HelloView {
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private Label welcometext;
+
+    @FXML
+    protected void onClickBoton() {
+        welcometext.setText("Hola " + txtNombre.getText() + "!");
+    }
+}
