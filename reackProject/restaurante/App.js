ab8f3c3d8ef945802a0efee44aea93f1783723af
BenjaminDiazP <20223tn048@utez.edu.mx>
2024-03-13 08:11:01 -0600

    Borre

--- reackProject/restaurante/App.js
+++
@@ -1,9 +0,0 @@
-import {} from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Navigation from "./src/config/navigation/Navigation";
-import { app, auth, db } from "./src/config/util/firebaseConecction.js";
-
-
-export default function App() {
-  return <Navigation />;
-}
